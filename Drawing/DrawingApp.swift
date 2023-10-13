268e149604fc253eef36b6673fa9649bc9ac189e
Venna Bharat <v.bharath39@gmail.com>
2023-10-14 04:32:01 +0530

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by bharat venna on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
